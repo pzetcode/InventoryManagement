03b775eb252dbfe2f966f0f4c909a43490a24320
Pawel Zet <pawel.zalewski.bsc@gmail.com>
2019-01-03 19:43:26 +0100

    Continue

--- InventoryManagement/ProductsForm.Designer.cs
+++ InventoryManagement/ProductsForm.Designer.cs
@@ -30,19 +30,20 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.inventorySystemDataSet = new InventoryManagement.InventorySystemDataSet();
             this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.inventorySystemDataSet = new InventoryManagement.InventorySystemDataSet();
             this.productsTableAdapter = new InventoryManagement.InventorySystemDataSetTableAdapters.ProductsTableAdapter();
             this.productIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.productNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.productStatusDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.inventorySystemDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventorySystemDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -50,20 +51,20 @@
             this.productNameDataGridViewTextBoxColumn,
             this.productStatusDataGridViewCheckBoxColumn});
             this.dataGridView1.DataSource = this.productsBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(70, 137);
+            this.dataGridView1.Location = new System.Drawing.Point(55, 66);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(342, 150);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // inventorySystemDataSet
-            // 
-            this.inventorySystemDataSet.DataSetName = "InventorySystemDataSet";
-            this.inventorySystemDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // productsBindingSource
             // 
             this.productsBindingSource.DataMember = "Products";
             this.productsBindingSource.DataSource = this.inventorySystemDataSet;
+            // 
+            // inventorySystemDataSet
+            // 
+            this.inventorySystemDataSet.DataSetName = "InventorySystemDataSet";
+            this.inventorySystemDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // productsTableAdapter
             // 
@@ -72,33 +73,33 @@
             // productIDDataGridViewTextBoxColumn
             // 
             this.productIDDataGridViewTextBoxColumn.DataPropertyName = "ProductID";
-            this.productIDDataGridViewTextBoxColumn.HeaderText = "ProductID";
+            this.productIDDataGridViewTextBoxColumn.HeaderText = "Product ID";
             this.productIDDataGridViewTextBoxColumn.Name = "productIDDataGridViewTextBoxColumn";
             // 
             // productNameDataGridViewTextBoxColumn
             // 
             this.productNameDataGridViewTextBoxColumn.DataPropertyName = "ProductName";
-            this.productNameDataGridViewTextBoxColumn.HeaderText = "ProductName";
+            this.productNameDataGridViewTextBoxColumn.HeaderText = "Product Name";
             this.productNameDataGridViewTextBoxColumn.Name = "productNameDataGridViewTextBoxColumn";
             // 
             // productStatusDataGridViewCheckBoxColumn
             // 
             this.productStatusDataGridViewCheckBoxColumn.DataPropertyName = "ProductStatus";
-            this.productStatusDataGridViewCheckBoxColumn.HeaderText = "ProductStatus";
+            this.productStatusDataGridViewCheckBoxColumn.HeaderText = "Product Status";
             this.productStatusDataGridViewCheckBoxColumn.Name = "productStatusDataGridViewCheckBoxColumn";
             // 
             // ProductsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(462, 450);
             this.Controls.Add(this.dataGridView1);
             this.Name = "ProductsForm";
             this.Text = "ProductsForm";
             this.Load += new System.EventHandler(this.ProductsForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.inventorySystemDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventorySystemDataSet)).EndInit();
             this.ResumeLayout(false);
 
         }
